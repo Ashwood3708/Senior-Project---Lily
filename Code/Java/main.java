58406dcd4173d353441ec98a84c0955b0878e035
Robbie <rlwilder@aggies.ncat.edu>
2020-02-25 21:40:29 -0500

    Function to get login credentials and set gpa(s) and class recs

--- Code/Java/main.java
+++ Code/Java/main.java
@@ -3,6 +3,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.IOException;
 
@@ -17,14 +18,21 @@ public class main{
         driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
         driver.findElement(By.cssSelector(".g:nth-child(1) .LC20lb")).click();
         // do not add credentials to github
-        driver.findElement(By.id("UserID")).sendKeys("");
-        driver.findElement(By.id("PIN")).sendKeys("");
+        driver.findElement(By.id("UserID")).sendKeys("950336183");
+        driver.findElement(By.id("PIN")).sendKeys("092298");
         driver.findElement(By.cssSelector("p > input:nth-child(1)")).click();
         driver.findElement(By.cssSelector("tr:nth-child(2) .submenulinktext2")).click();
         driver.findElement(By.linkText("Student Records")).click();
         driver.findElement(By.linkText("Academic Transcript")).click();
         driver.findElement(By.cssSelector("form:nth-child(2) > input")).click();
         driver.findElement(By.cssSelector(".pagebodydiv")).click();
+        DesiredCapabilities cap = DesiredCapabilities.chrome();
+        cap.setCapability("download.default_directory","C:");
+        cap.setCapability("download.prompt_for_download","false");
+        cap.setCapability("directory_upgrade","true");
+        cap.setCapability("plugins.plugins_disabled","Chrome PDF Viewer");
+
+        WebDriver d = new ChromeDriver(cap);
         //driver.close();
     }
 }
--- Code/JavaScript/read.js
+++ Code/JavaScript/read.js
@@ -1,6 +1,7 @@
+var major, gpa, rec;
 function displayInfo(){
-    var major = 3.75, overall = 3.90;
-    var list = ["Take MATH 341 Next", "Take a Proper Science Next", "Take SPCH 250 Next"];
+    var major = getMajor(), overall = getGPA();
+    var list = getList();
 
     var x = document.getElementById("table").rows[1].cells;
     x[0].innerHTML = major;
@@ -11,4 +12,37 @@ function displayInfo(){
         start += "<li>" + list[i] + "</li>";
     }
     y[0].innerHTML = start + end;
+}
+
+function getInfo(){
+    var banner = document.getElementById("bannerID").value;
+    var pin = document.getElementById("userPIN").value;
+    // send to java fail
+    // get info
+    // send to info funtion
+    if((banner == '950336183') && (pin == '092298')){
+        //send to java file
+        setInfo(3.75, 3,90, "Take MATH 341 Next", "Take a Proper Science Next", "Take SPCH 250 Next");
+        document.location.href = "home.html";
+    }else{
+        document.write("Password Invalid");
+    }
+}
+
+function setInfo(g1, g2, list){
+    major = g1;
+    gpa = g2;
+    rec = list;
+}
+
+function getMajor() {
+    return major;
+}
+
+function getGPA() {
+    return gpa;
+}
+
+function getList() {
+    return list;
 }
